a3de6999551a59646a8b098fcacb517cf4136227
root <root@TestServer.localdomain>
2015-02-19 13:48:31 +0900

    fix: ansible path

--- ansible-test.sh
+++ ansible-test.sh
@@ -1,1 +1,1 @@
-cd /ansible-pyenv ; ansible-playbook /ansible-pyenv/tests/test.yml -i /ansible-pyenv/tests/inventory -c local --sudo
+cd /ansible-pyenv ; /opt/ansible/ansible/bin/ansible-playbook /ansible-pyenv/tests/test.yml -i /ansible-pyenv/tests/inventory -c local --sudo
