b8c47662c0a43ea465e751723ee3152dee89ee78
shogito <pijo.eng@gmail.com>
2015-02-23 13:08:02 +0900

    fix: typo

--- tests/ansible-test.sh
+++ tests/ansible-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /ansible-pyenv
 
-#cat <<EOF > ansible.cfg
-#[defaults]
-#roles_path = ../
-#EOF
+cat <<EOF > ansible.cfg
+[defaults]
+roles_path = ../
+EOF
 
 /opt/ansible/ansible/bin/ansible-playbook /ansible-pyenv/tests/test.yml -i /ansible-pyenv/tests/inventory -c local --sudo
 
