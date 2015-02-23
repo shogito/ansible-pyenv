104c97be13c5cc328d60e24434554d25358b36f5
shogito <pijo.eng@gmail.com>
2015-02-23 11:17:33 +0900

    fix: test script

--- tests/ansible-test.sh
+++ tests/ansible-test.sh
@@ -1,1 +1,5 @@
-cd /ansible-pyenv ; echo '[defaults]' > ansible.cfg ; echo 'roles_path = ../' >> ansible.cfg ; /opt/ansible/ansible/bin/ansible-playbook /ansible-pyenv/tests/test.yml -i /ansible-pyenv/tests/inventory -c local --sudo
+#!/bin/bash
+cd /ansible-pyenv
+echo '[defaults]' > ansible.cfg
+echo 'roles_path = ../' >> ansible.cfg 
+/opt/ansible/ansible/bin/ansible-playbook /ansible-pyenv/tests/test.yml -i /ansible-pyenv/tests/inventory -c local --sudo
